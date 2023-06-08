75949f49af30d4384bd7df7dd2991b7ddcf67e5d
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-08 07:41:14 +0300

    add simple client side routing

--- src/client/client.js
+++ src/client/client.js
@@ -1,9 +1,15 @@
 // Startup point for the client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
 // Here we use a special 'hydrate' method of ReactDOM to work with SSR markup in the browser.
 // This process of adding JS interactivity back to the static markup (which is more of a preview before this step)
 // and re-rendering it afterward is called HYDRATION.
-ReactDOM.hydrate(<Home />, document.getElementById('root'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
