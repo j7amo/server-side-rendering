58692775020b102dcc7adbe592883ac1cca62543
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-08 07:57:35 +0300

    add simple server side routing to match the client side routing

--- src/helpers/renderHtml.js
+++ src/helpers/renderHtml.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import Home from '../client/components/Home';
+import { StaticRouter } from 'react-router-dom';
+import Routes from '../client/Routes';
 
-export default () => {
+export default (req) => {
   // The problem is: Node environment does not know anything about JSX.
   // So if we try to execute index.js without additional setup,
   // we will get: SyntaxError: Unexpected token '<'
-  const content = renderToString(<Home />);
+  const content = renderToString(
+    // StaticRouter cannot look at the address bar like BrowserRouter does,
+    // so we need to pass the current path user is trying to visit via 'location' prop.
+    // The path can be found on a 'req' object of Express route handler.
+    <StaticRouter location={req.path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  );
 
   // We need to include ONLY the client side JS in the resulting HTML
   // to let browser know that it ALSO needs to download the script file for the app to work properly.
--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ const app = express();
 // because 'bundle.js' file with all the clientside JS code is located in this folder.
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+// We are using '*' here to let Express catch ANY route BUT not really handling it.
+// No matter what path is requested ('/banana', '/123' etc.) by user,
+// Express will behave exactly the same:
+// it will send back the result of calling 'renderHtml' function that WILL really handle
+// routing because it uses StaticRouter under the hood.
+app.get('*', (req, res) => {
   // Here we only send resulting HTML back to the client
   // NO JS! We need to fix it!
-  res.send(renderHtml());
+  res.send(renderHtml(req));
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
