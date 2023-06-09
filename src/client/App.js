21349041c1a332317634033fede5d204e7678484
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-09 17:08:04 +0300

    add fetching of auth status to the App component on the server side

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
 import Header from './components/Header';
+import { fetchCurrentUser } from './actions';
 
 // Because we have nested routes that we want to render inside an App component
 // we need to use a special 'route' prop that has 'routes' property on it.
@@ -13,4 +14,10 @@ const App = ({ route }) => {
   );
 };
 
-export default { component: App };
+export default {
+  component: App,
+  // Add 'loadData' property (just like we did with other components that need data fetching).
+  // Why do we add fetching of current user auth status to the App component?
+  // Because we want to always check it and the App component is rendered 100% of the time.
+  loadData: ({ dispatch }) => dispatch(fetchCurrentUser()),
+};
