af65ca1134c0d840fb862f800fdb606add46f32c
roman.tarnakin <roman.tarnakin@gmail.com>
2023-06-10 11:09:02 +0300

    add promise.all hack

--- src/index.js
+++ src/index.js
@@ -71,35 +71,58 @@ app.get('*', async (req, res) => {
   // and returns the array of components to be rendered.
   // So by now we know exact components to be rendered WITHOUT rendering them (remember that we want
   // to 1) fetch all the needed data 2) render the app ONLY once 3) return the string to the client).
-  const pendingRequests = matchRoutes(Routes, req.path).map((matchedRoute) => {
-    if (matchedRoute.route.loadData) {
-      return matchedRoute.route.loadData(store);
-    }
-  });
+  const pendingRequests = matchRoutes(Routes, req.path)
+    .map((matchedRoute) => {
+      if (matchedRoute.route.loadData) {
+        return matchedRoute.route.loadData(store);
+      }
+
+      return null;
+    })
+    // Because we want to somehow resolve the issue of Promise.all calling 'catch' too early
+    // we can use a HACK here:
+    .map((pendingRequest) => {
+      if (pendingRequest) {
+        // HACK: we wrap the promise with ANOTHER promise and resolve it no matter what (even if it was rejected)
+        // This will result in Promise.all NEVER calling the 'catch' method!
+        return new Promise((resolve, reject) => {
+          pendingRequest.then(resolve).catch(resolve);
+        });
+      }
+    });
 
   // 'pendingRequests' is an array of promises because 'loadData' function returns a promise.
   // To make sure that we
   // 1) fetch all the data and
   // 2) update store with this data before rendering - we await 'Promise.all'
-  await Promise.all(pendingRequests);
+  try {
+    // The PROBLEM with Promise.all:
+    // Let's say we have 10 pending requests (promises) and one of them gets rejected. This means that the remaining 9
+    // promises will not be awaited at all and the whole Promise.all will immediately call the 'catch' function OR
+    // end up in the 'catch' statement if we are using 'try-catch' approach. And this is not a good situation for us
+    // because we would still like to render ALL AVAILABLE CONTENT. But with this approach we CANNOT do it!
+    await Promise.all(pendingRequests);
 
-  // Here we define an object that will be used by StaticRouter.
-  // StaticRouter will pass this object to any component it renders.
-  // So the idea is the following:
-  // 1) pass 'context' to StaticRouter
-  // 2) StaticRouter passes it to NotFoundPage
-  // 3) inside NotFoundPage component add an error property to the context object
-  // 4) after HTML markup was created AND before sending it back to the client
-  // CHECK context object for error property and send 404 Not Found status if error is present.
-  const context = {};
+    // Here we define an object that will be used by StaticRouter.
+    // StaticRouter will pass this object to any component it renders.
+    // So the idea is the following:
+    // 1) pass 'context' to StaticRouter
+    // 2) StaticRouter passes it to NotFoundPage
+    // 3) inside NotFoundPage component add an error property to the context object
+    // 4) after HTML markup was created AND before sending it back to the client
+    // CHECK context object for error property and send 404 Not Found status if error is present.
+    const context = {};
 
-  // And now we have everything we need to create markup and send it to the client
-  const html = renderHtml(req, store, context);
+    // And now we have everything we need to create markup and send it to the client
+    const html = renderHtml(req, store, context);
 
-  if (context.notFound) {
-    res.status(404).send(html);
-  } else {
-    res.send(html);
+    if (context.notFound) {
+      res.status(404).send(html);
+    } else {
+      res.send(html);
+    }
+  } catch (err) {
+    console.log('Error!');
   }
 });
 
